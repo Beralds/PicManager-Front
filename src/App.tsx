7c6a1915ed417b3bd3946f6c6ba8ea785427b544
Wender Lemes <wender.lemes@dtidigital.com.br>
2025-01-24 17:43:30 -0300

    Improving navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,51 @@ import LandingPage from "./pages/landingPage";
 import MyUsersPage from "./pages/myUsersPage";
 import UserModel from "./@types/userModel";
 import MyAlbumsPage from "./pages/myAlbumsPage";
+import { Page } from "./enums/pages";
+import MyPhotosPage from "./pages/myPhotosPage";
+// import MyPhotosPage from "./pages/myPhotosPage";
 
 export function App() {
+  const [currentPage, setCurrentPage] = useState(Page.Landing);
   const [currentUserEmail, setCurrentUserEmail] = useState('');
   const [selectedUser, setSelectedUser] = useState<UserModel>();
   // const [selectedAlbumId, setSelectedAlbumId] = useState<number>();
   const allowChanges = currentUserEmail === selectedUser?.email;
 
+  const handleSignIn = (userEmail: string) => {
+    setCurrentUserEmail(userEmail);
+    setCurrentPage(Page.Users);
+  }
+  const handleSelectUser = (user: UserModel) => {
+    setSelectedUser(user);
+    setCurrentPage(Page.Albums);
+  }
+  const handleSelectAlbum = () => {
+    setCurrentPage(Page.Photos);
+  }
+
   return (
-    <>
-      { !currentUserEmail ? 
+    <div className="flex flex-col justify-center items-center">
+      <div className="bg-gray-950 w-[100%] h-[60px] mb-2 flex justify-between items-center">
+        <h1>PIC MANAGER</h1>
+        <div>Oi</div>
+      </div>
+      { currentPage === Page.Landing ? 
         <LandingPage 
-          handleSubmit={(userEmail) => setCurrentUserEmail(userEmail)} 
+          handleSubmit={handleSignIn} 
         /> :  null }
-      { currentUserEmail && !selectedUser ? 
+      { currentPage === Page.Users ? 
         <MyUsersPage 
-          handleOpenUserAlbum={(user) => setSelectedUser(user)} 
+          handleOpenUserAlbum={handleSelectUser} 
         /> :  null }
-      { selectedUser ? 
+      { currentPage === Page.Albums ? 
         <MyAlbumsPage 
           allowChanges={allowChanges}
         /> :  null }
-    </>
+      { currentPage === Page.Photos ? 
+        <MyPhotosPage 
+          allowChanges={allowChanges}
+        /> :  null }
+    </div>
   )
 }
---
+++ src/enums/pages.tsx
@@ -0,0 +1,6 @@
+export enum Page {
+  Landing,
+  Users,
+  Albums,
+  Photos,
+}
